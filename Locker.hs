d880cb1fdd7718f37b25d418d4017b65ed54c187
Roslyn Parker <magicmachineplaymc@gmail.com>
2020-04-13 10:51:51 -0400

    finished!

--- Locker.hs
+++ Locker.hs
@@ -1,6 +1,6 @@
 -- Roslyn Parker
 -- Programming Languages
--- 8 April 2020
+-- 13 April 2020
 module Example.Locker 
 (Locker, unlock, lock)
 where
@@ -9,22 +9,22 @@ import Control.Monad
 import Data.Maybe
 import Data.Functor
 
-data Locker = Locker { key :: Maybe Int
-                     , value :: Maybe Int}
+data Locker a = Locker { key :: Int
+                     , value :: a}
 
-instance Show Locker where
+instance Show (Locker a) where
     show locker = "*SECRET*"
 
 instance Functor Locker where
-     fmap f l = Locker {key = key l, value = f (value l)}
+     fmap f Locker {key = k, value = v} = Locker {key = k, value = f v}
 
 -- lock takes a key and a value
 -- returns a Locker
-lock :: Int -> Int -> Locker
-lock k v = Locker {key = Just k, value = Just v}
+lock :: Int -> a -> Locker a
+lock k v = Locker {key = k, value = v}
 
 -- unlock returns the value only if the key matches
 -- returns Nothing if the key doesn't match
 -- returns Just value if the key matches
-unlock :: Maybe Int -> Locker -> Maybe Int
-unlock k l = if key l == k then value l else Nothing
+unlock :: Int -> Locker a -> Maybe a
+unlock k l = if key l == k then Just (value l) else Nothing
