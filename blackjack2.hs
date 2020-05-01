e0c3fa7ad240f8e6fad63262312fc95d7b09cd0b
Roslyn Parker <magicmachineplaymc@gmail.com>
2020-05-01 13:46:41 -0400

    almost finished

--- blackjack2.hs
+++ blackjack2.hs
@@ -10,23 +10,57 @@ main = do
     gen <- newStdGen 
     let deck = shuffle gen fullDeck
     let ([card1, card2], newDeck) = drawHand 2 deck
-    let ([card], newDeck') = drawHand 1 newDeck
+    let ([downCard, upCard], newDeck') = drawHand 2 newDeck
 
     let firstOutput = "Your first card is " ++ show card1
     let secondOutput = "Your second card is " ++ show card2
-    let dOutput = "Dealer's up card is " ++ show card
+
+    let total = totalCard (sumCards (valueCard [card1, card2]))
+    let dOutput = "Dealer's up card is " ++ show upCard
 
     putStrLn firstOutput
     putStrLn secondOutput
     putStrLn dOutput
 
+    hos newDeck' [card1, card2] [downCard, upCard]
+
     return (0)
 
 -- Hit Or Stand
-hos :: String -> Deck -> ([Card], Deck)
+hos :: Deck -> [Card] -> [Card] -> IO
+hos deck cards dcards = do
+    let prompt = "Hit or stand?"
+    putStrLn prompt
+    input <- getLine
+    if input == "hit" then do 
+        let ([card], newDeck) = drawHand 1 deck
+        let output = "Your new card is " ++ show card
+        putStrLn output
+        hos newDeck cards:card dcards
+    else do
+        let total = totalCard (sumCards (valueCard cards))
+        let poutput = "Your total is " ++ show total
+        let outputd = "Dealer's down card is " ++ show (head dcards)
+        putStrLn poutput
+        putStrLn outputd
+        dealerTurn deck cards dcards
 
--- Dealer hand check
-dealerHand :: Deck -> ([Card], Deck)
+dealerTurn :: Deck -> [Card] -> [Card] -> IO
+dealerTurn deck cards dcards = do 
+            let total = totalCard (sumCards (valueCard dcards))
+            if  total < Just 17 then do 
+                 let ([dcard], newDeck) = drawHand 1 deck
+                 let output = "Dealer draws " ++ show dcard
+                 putStrLn output
+                 dealerTurn newDeck cards dcards:dcard
+            else do 
+                if total == Nothing then do 
+                    let poutput = "Dealer busts!"
+                    putStrLn poutput
+                else do
+                    let ptotal = totalCard (sumCards (valueCard cards))
+                    let doutput = "Dealer wins " ++ show total ++ " to " ++ show ptotal
+                    putStrLn doutput
 
 -- Draw a hand of cards for blackjack game
 drawHand :: Int -> Deck ->([Card],Deck)
@@ -34,9 +68,6 @@ drawHand 0 deck = ([], deck)
 drawHand n deck = let (card, newDeck) = draw deck in
                   let (hand, newDeck') = drawHand (n - 1) newDeck in
                   (card:hand, newDeck')
-
---     add applicative sum (+) <$> list1 <*> list2
-
 
 -- Finds all values of cards in hand
 valueCard :: [Card] -> [[Int]]
